a4df95f51489cdbace935203ea4f931f96b3c616
Vaibhav Jain <vaibhav.jain01@nagarro.com>
2022-03-09 23:51:39 +0530

    Add project files.

---
+++ BathanysPieShop/Controllers/PieController.cs
@@ -0,0 +1,28 @@
+﻿using BathanysPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BathanysPieShop.Controllers
+{
+    public class PieController: Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly ICategoryRepository _categoryRepository;
+
+        public PieController(IPieRepository pieRepository, ICategoryRepository categoryRepository)
+        
+        {
+            _pieRepository = pieRepository;
+            _categoryRepository = categoryRepository;
+        }
+
+        public ViewResult List()
+        {
+            ViewBag.CurrentCategory = "Saturday Cakes";
+            return View(_pieRepository.AllPies);
+        }
+    }
+}
---
+++ BathanysPieShop/Models/MockPieRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BathanysPieShop.Models
+{
+    public class MockPieRepository : IPieRepository
+    {
+        private readonly ICategoryRepository _categoryRepository = new MockCategoryRepository();
+        public IEnumerable<Pie> AllPies => 
+            new List<Pie> { 
+            
+                new Pie { PieId =1, Name="Strawberry Pie", Price = 15.95M, ShortDescription="Strawberry pie description.", Category =_categoryRepository.AllCategories.SingleOrDefault(c=>c.CategoryName.Equals("Fruit Pies"))},
+                new Pie {PieId =2, Name="Cheese Cake", Price=18.95M, ShortDescription = "CheeseCakeDescription", Category = _categoryRepository.AllCategories.SingleOrDefault(c=>c.CategoryName.Equals("Cheese Cakes"))},
+                new Pie { PieId =3, Name="Rhubarb Pie", Price = 15.95M, ShortDescription="Rhubarb Pie description.", Category = _categoryRepository.AllCategories.SingleOrDefault(c=>c.CategoryName.Equals("Fruit Pies"))},
+                new Pie {PieId =4, Name="Pumpkin Cake", Price=18.95M, ShortDescription = "PumpkinCakeDescription", Category = _categoryRepository.AllCategories.SingleOrDefault(c=>c.CategoryName.Equals("Seasonal Pies"))}
+            };
+
+        public IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        public Pie GetPieById(int pieId)
+        {
+            return AllPies.FirstOrDefault(p=>p.PieId == pieId);
+        }
+    }
+}
